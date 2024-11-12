e3ff1698b24400925809df006dd481a9811bfcad
Andrew Ganzha <jackbrikman@mail.ru>
2024-11-12 17:02:24 +0300

    small fix

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,45 @@ import Header from "./components/organism/Header.vue";
 <template>
   <div class="container">
     <Header />
-
-    <main>
-      <div class="content">
-        <router-view/>
-      </div>
-    </main>
   </div>
+
+    <main class="main">
+      <img src="./assets/img/rectangle.svg" alt="rectangle" class="upRectangle"/>
+      <div class="container">
+        <div class="content">
+          <router-view/>
+        </div>
+      </div>
+      <img src="./assets/img/rectangle.svg" alt="rectangle" class="bottomRectangle"/>
+    </main>
 </template>
 
 <style>
+.main {
+  position: relative;
+}
+
+.upRectangle {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.bottomRectangle {
+  transform: rotate(180deg);
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
 .container {
-  max-width: 1440px;
   margin: 0 auto;
+  max-width: 1440px;
 }
 
 .content {
+  padding-top: 3rem;
+  height: 100vh;
   margin: 0 auto;
 }
 </style>
--- src/components/organism/Contacts.vue
+++ src/components/organism/Contacts.vue
@@ -43,16 +43,45 @@ import {contactsData} from "../../assets/data/contacts.ts";
   <div :class="$style.Contacts">
       <Label>Контакты:</Label>
 
-      <div v-for="contact in contactsData">
-          <span><Label>{{contact.title}}</Label>
+      <div v-for="contact in contactsData" :class="$style.contact">
+        <span :class="$style.title">{{contact.title}}</span>
 
-            <Label>{{ contact.subtitle }}</Label></span>
+        <span>{{ contact.subtitle }}</span>
       </div>
   </div>
+
+  <img :class="$style.map" src="../../assets/img/map.png" alt="map"/>
 </template>
 
 <style module lang="scss">
 .Contacts {
-  //
+  margin: 0 auto;
+  width: fit-content;
+  height: 100%;
+  position: relative;
+  font-size: 2.4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  line-height: 3rem;
+}
+
+.map {
+  width: 100vw;
+  height: auto;
+  position: absolute;
+  left: 0;
+  z-index: 2;
+  bottom: 0;
+}
+
+.contact {
+  text-align: left;
+  display: flex;
+  gap: 0.4rem;
+}
+
+.title {
+  font-weight: 500;
 }
 </style>
